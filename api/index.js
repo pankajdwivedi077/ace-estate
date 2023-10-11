3a01c490136cc57aee35d0362c7d3cf2233d81cc
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2023-10-11 17:26:20 +0530

    create routes and middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
+const app = express();
+
+app.use(express.json());
 
 dotenv.config();
 
@@ -13,8 +19,21 @@ mongoose
     console.log(err);
   });
 
-const app = express();
+app.use('/api/user', userRouter);
+
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
 
 app.listen(3000, () => {
-  console.log("Server is running on Port 3000");
+  console.log('Server is running on Port 3000');
 });
+
