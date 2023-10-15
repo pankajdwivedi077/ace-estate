b02d1a5cda25d146d14022654562f0510447b0c1
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2023-10-15 18:03:12 +0530

    update api

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 dotenv.config();
 
