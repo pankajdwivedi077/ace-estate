0c05337538c94615ffeb61003d0608ce5c803b46
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2023-10-18 14:26:28 +0530

    user listing delete func

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { creteListing } from '../controllers/listing.controller.js'
+import { creteListing, deleteListing } from '../controllers/listing.controller.js'
 import { verifyToken } from '../utils/VerifyUser.js'
 
 const router = express.Router()
 
 router.post('/create', verifyToken, creteListing)
+router.delete('/delete/:id', verifyToken, deleteListing)
 
 export default router;
