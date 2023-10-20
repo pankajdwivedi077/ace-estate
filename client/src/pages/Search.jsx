353da09ee7b690f66a4a7ca43ba6b74c0921f904
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2023-10-20 14:22:25 +0530

    add show more

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -57,7 +57,7 @@ export default function Search () {
             if (data.length > 8){
               setShowMore(true);
             }else{
-              setShowMore(false)
+              setShowMore(false) 
             }
             setListings(data);
             setLoading(false);
