ba9f9310e6643640cd586f222fed1fc1f92f5622
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2023-10-20 16:44:31 +0530

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 export default function About(){
     return (
-        <div>
-            About
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+           <h1 className="text-exl font-bold mb-4 text-slate-900">About Ace Estate </h1> 
+           <p className="mb-4 text-slate-700">Ace Estate is a cutting-edge real estate platform that brings simplicity and efficiency to the process of finding your perfect place to live. </p>
+           <p className="mb-4 text-slate-700">
+            1. Property Listings: Explore a wide range of property listings that cater to all your housing needs, whether you're looking to buy, rent, or simply curious about the market.
+           </p>
+           <p className="mb-4 text-slate-700">2. User-Friendly Interface: Our user-friendly interface ensures a seamless browsing experience, making it easy to find and compare properties</p>
+           <p className="mb-4 text-slate-700">
+           Ace Estate is committed to simplifying the way you discover your perfect place to live. Our mission is to make the entire process of buying or renting a property as smooth and enjoyable as possible. Welcome to Ace Estate, where you'll find a home that suits your unique lifestyle and preferences.
+
+Discover your future home with Ace Estate today!
+           </p>
         </div>
     )
 }
